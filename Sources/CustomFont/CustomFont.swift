c89c20cbf5c10101c9ab3295048cd9e84e5b19d8
ludo <ludovicgrimbert@orange.fr>
2020-12-14 18:17:59 +0100

    First commit

--- Sources/CustomFont/CustomFont.swift
+++ Sources/CustomFont/CustomFont.swift
@@ -1,3 +1,39 @@
-struct CustomFont {
-    var text = "Hello, World!"
+//
+//  CustomFont.swift
+//
+//  Created by Ludovic Grimbert on 06/05/2020.
+//  Copyright © 2020 ludovic grimbert. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+@available(iOS 13, macCatalyst 15, watchOS 7, *)
+struct CustomFont: ViewModifier {
+    @Environment(\.sizeCategory) var sizeCategory
+
+    var name: String
+    var style: UIFont.TextStyle
+    var weight: Font.Weight = .regular
+
+    func body(content: Content) -> some View {
+        return content.font(Font.custom(
+            name,
+            size: UIFont.preferredFont(forTextStyle: style).pointSize)
+            .weight(weight))
+    }
 }
+
+@available(iOS 13, macCatalyst 15, watchOS 7, *)
+extension View {
+    func customFont(
+        name: String,
+        style: UIFont.TextStyle,
+        weight: Font.Weight = .regular) -> some View {
+        return self.modifier(CustomFont(name: name, style: style, weight: weight))
+    }
+}
+
+// USE example :
+//.customFont(name: "Helvetica Neue", style: .title2, weight: .light)
+
--- Tests/CustomFontTests/CustomFontTests.swift
+++ Tests/CustomFontTests/CustomFontTests.swift
@@ -6,7 +6,7 @@ final class CustomFontTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(CustomFont().text, "Hello, World!")
+      //  XCTAssertEqual(CustomFont().text, "Hello, World!")
     }
 
     static var allTests = [
